a6618116f8111a63d33678959152b6841699ea4a
manh14 <manh140405@gmail.com>
2025-05-26 23:22:24 +0700

    làm navbar

--- src/Component/Navbar/navbar.js
+++ src/Component/Navbar/navbar.js
@@ -1,10 +1,20 @@
-//import React from 'react'
+import React, { useState } from 'react';
 import './navbar.css';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
 import YouTubeIcon from '@mui/icons-material/YouTube';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import PersonIcon from '@mui/icons-material/Person';
 const Navbar = () => {
+    const [userPic,setUserpic] = useState("https://th.bing.com/th/id/R.9616f533f2174933ec04cc74164108f9?rik=Ucnk5KRnhC4duw&riu=http%3a%2f%2fwww.bugando.ac.tz%2fschools%2fSchool_Of_Pharmacy%2fimages%2fteams%2favatar.jpg&ehk=Na8vu%2fHzbfviY3bYO%2fRUXOHZ8JqNHAb8OcJ3fiz%2bpME%3d&risl=&pid=ImgRaw&r=0")
+    const [navbarModal,setNavbarModal] = useState(false);
+    
+    const handleClickModal =()=>{
+        setNavbarModal(prev=>!prev);
+    }
+
     return (
         <div className='navbar'>
 
@@ -21,10 +31,31 @@ const Navbar = () => {
             <div className="navbar-middle">
                 <div className="navbar_searchBox">
                     <input type='text' placeholder='Search' className='navbar_searchBoxInput' />
+                    <div className="navbar_searchIconBox"><SearchIcon sx={{fontSize:"28px",color:"white"}} /></div>    
                 </div>
+
+                <div className="navbar_mike">
+                    <KeyboardVoiceIcon sx={{color:"white"}}/>
+                </div>
+            </div>
+
+            <div className="navbar-right">
+                <VideoCallIcon sx={{fontSize:"30px", cursor:"pointer", color:"white"}}/>
+                <NotificationsActiveIcon sx={{fontSize:"30px", cursor:"pointer", color:"white"}}/>
+                <img onClick={handleClickModal} src={userPic} className="navbar-right-logo" alt="Logo"  />
+
+                { navbarModal &&
+                    <div className="navbar-modal">
+                    <div className="navbar-modal-option">Profile</div>
+                    <div className="navbar-modal-option">Logout</div>
+                    <div className="navbar-modal-option">Login</div>
+
+                </div>
+                }
             </div>
         </div>
     )
 }
 
 export default Navbar
+
